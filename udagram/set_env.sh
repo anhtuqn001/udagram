71d13d322a1a61988768d2fd585cf2b55150e4c9
anhtu.tran <anhtu.tran@lgepartner.com>
2023-09-15 00:21:09 +0700

    Sync up commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Tu0342669355
+export POSTGRES_HOST=udagram-db.ctzkpmlgpu0z.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udagram-tuta17
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
