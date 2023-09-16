6915d3ac1f2adef00a4e36c644c697407875272e
anhtu.tran <anhtu.tran@lgepartner.com>
2023-09-16 23:05:58 +0700

    Update

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -22,7 +22,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   await sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
 
-  console.log("Database Connectedd");
+  console.log("Database Connected");
 
   const app = express();
   const port = 8080;
