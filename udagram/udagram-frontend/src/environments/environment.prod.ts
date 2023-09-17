b34e8227a01aa13c89763f53c37e804bfa3fcea1
anhtu.tran <anhtu.tran@lgepartner.com>
2023-09-17 22:36:45 +0700

    update

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://tuta17-udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
